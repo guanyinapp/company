609d12f694d58db9e2c9413131a8e88dfdd3b924
gwin <guanyinapp@gmail.com>
2018-03-14 14:22:40 +0800

    m2

--- app/src/main/java/com/guanyin/handlertest/MainActivity.java
+++ app/src/main/java/com/guanyin/handlertest/MainActivity.java
@@ -77,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void run() {
 
-            txt1.setText("drango");
+            txt1.setText("ffff");
         }
     }
 }
